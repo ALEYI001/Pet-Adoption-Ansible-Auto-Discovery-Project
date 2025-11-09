02a637284fe594c2b6a114136d032f3b05a98c3a
Chijiokeproject <alaukwu.chijioke@cloudhight.com>
2025-11-09 22:05:04 +0000

    ansible server

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ module "vpc" {
   source = "./module/vpc"
   name = local.name
 }
+
+module "ansible" {
+  source              = "./module/ansible"
+  name                = local.name
+  vpc_id              = module.vpc.vpc_id
+  subnet_id           = [module.vpc.private_subnet_ids[0]]
+  keypair_name        = module.vpc.keypair_name
+  private_key         = module.vpc.private_key
+  newrelic_api_key    = var.newrelic_api_key
+  newrelic_account_id = var.newrelic_account_id
+}
--- provider.tf
+++ provider.tf
@@ -3,13 +3,13 @@ provider "aws" {
   profile = "pet_team"
 }
 
-terraform {
-  backend "s3" {
-    bucket       = "adoptionteam1-bucket"
-    key          = "infra/terraform.tfstate"
-    region       = "us-east-1"
-    profile      = "pet_team"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket       = "adoptionteam1-bucket"
+#     key          = "infra/terraform.tfstate"
+#     region       = "us-east-1"
+#     profile      = "pet_team"
+#     encrypt      = true
+#     use_lockfile = true
+#   }
+# }
