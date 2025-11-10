e3a1bfcdb7fd02c21fffe51244bb6a4630d1f089
onyiafranklin <onyiafranklin89@gmail.com>
2025-11-10 20:40:29 +0000

    update

--- main.tf
+++ main.tf
@@ -17,3 +17,13 @@ module "ansible" {
   newrelic_api_key    = var.newrelic_api_key
   newrelic_account_id = var.newrelic_account_id
 }
+module "database" {
+  source      = "./module/database"
+  name        = local.name
+  db_subnets  = [module.vpc.private_subnet_ids[0], module.vpc.private_subnet_ids[1]]
+  vpc_id      = module.vpc.vpc_id
+  stage_sg    = module.prod.prod_sg
+  prod_sg     = module.stage.stage_sg
+  db_username = data.vault_generic_secret.database.data["username"]
+  db_password = data.vault_generic_secret.database.data["password"]
+}
