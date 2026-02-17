fb34fa40e4b3036c775d0a7c5258b5a2101916e5
Inalegwu ALEYI <johnaleyi1@gmail.com>
2026-02-17 01:53:30 +0000

    Add Terraform variables for region and New Relic

---
+++ utility/variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+#domain
+variable "domain_name" {
+  default = "aleyi.space"
+}
+
+variable "newrelic_api_key" {
+  default = "NRAK-HM7WE8XXY3PAZR5LBG2AHC22PYH"
+}
+
+variable "newrelic_account_id" {
+  default = "7233367"
+}
